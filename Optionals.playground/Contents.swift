f46290d230568ca8a652d30d99ccd6eda16296ee
henrykchan <chan.henryk@gmail.com>
2016-09-23 17:23:06 -0400

    Optionals Complete

---
+++ Optionals.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var petName: String?
+
+petName = "Scooter"
+
+print(petName)
+
+
+if petName != nil {
+    print("My friend's pet is \(petName)")
+} else {
+    print("My friend doesn't have a pet :(")
+}
+
+
+if let petName = petName {
+    print("My friend's pet is \(petName)")
+}
+
+print (petName)
+
+if let friendsPet = petName {
+    print("My friend's pet is \(friendsPet)")
+    print("petName is still an Optional! \(petName)")
+}
